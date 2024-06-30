606f391f05b3ff83547322d1d50a2884bbd1a2bb
Ilnaz Khasanov <ilnaz.hasanoff2003@gmail.com>
2024-06-30 22:00:59 +0300

    feat: add base class for simulation command event args

---
+++ src/SimulationStorm.Simulation.Presentation/SimulationManager/SimulationCommandEventArgs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimulationStorm.Simulation.Presentation.SimulationManager;
+
+public class SimulationCommandEventArgs(SimulationCommand command) : EventArgs
+{
+    public SimulationCommand Command { get; } = command;
+}
