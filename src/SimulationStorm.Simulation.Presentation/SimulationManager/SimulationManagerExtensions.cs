cbd2019ee2f1bc0dbdc0cfb634b997b4e47bdb6b
Ilnaz Khasanov <ilnaz.hasanoff2003@gmail.com>
2024-06-30 22:39:26 +0300

    feat(simulation-manager)!: add simulation manager extensions class

---
+++ src/SimulationStorm.Simulation.Presentation/SimulationManager/SimulationManagerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SimulationStorm.Simulation.Presentation.SimulationManager;
+
+public static class SimulationManagerExtensions
+{
+    public static IObservable<SimulationCommandEventArgs> CommandScheduledObservable
+    (
+        this ISimulationManager simulationManager)
+    {
+        return Observable
+            .FromEventPattern<EventHandler<SimulationCommandEventArgs>, SimulationCommandEventArgs>
+            (
+                h => simulationManager.CommandScheduled += h,
+                h => simulationManager.CommandScheduled -= h
+            )
+            .Select(ep => ep.EventArgs);
+    }
+    
+    public static IObservable<SimulationCommandEventArgs> CommandStartingObservable
+    (
+        this ISimulationManager simulationManager)
+    {
+        return Observable
+            .FromEventPattern<EventHandler<SimulationCommandEventArgs>, SimulationCommandEventArgs>
+            (
+                h => simulationManager.CommandStarting += h,
+                h => simulationManager.CommandStarting -= h
+            )
+            .Select(ep => ep.EventArgs);
+    }
+    
+    public static IObservable<SimulationCommandCompletedEventArgs> CommandCompletedObservable
+    (
+        this ISimulationManager simulationManager)
+    {
+        return Observable
+            .FromEventPattern<EventHandler<SimulationCommandCompletedEventArgs>, SimulationCommandCompletedEventArgs>
+            (
+                h => simulationManager.CommandCompleted += h,
+                h => simulationManager.CommandCompleted -= h
+            )
+            .Select(ep => ep.EventArgs);
+    }
+}
