22d6ba1c8646ff2dd0ceca0c04ce32ad5e0ab814
Ilnaz <ilnaz.hasanoff2003@gmail.com>
2024-07-01 09:44:56 +0300

    refactor(utils)!: make BenchmarkResult struct

--- src/SimulationStorm.Utilities/Benchmarking/BenchmarkResult.cs
+++ src/SimulationStorm.Utilities/Benchmarking/BenchmarkResult.cs
@@ -2,7 +2,7 @@
 
 namespace SimulationStorm.Utilities.Benchmarking;
 
-public class BenchmarkResult(TimeSpan elapsedTime)
+public readonly struct BenchmarkResult(TimeSpan elapsedTime)
 {
     public TimeSpan ElapsedTime { get; } = elapsedTime;
 }
--- src/SimulationStorm.Utilities/Benchmarking/BenchmarkResult`1.cs
+++ src/SimulationStorm.Utilities/Benchmarking/BenchmarkResult`1.cs
@@ -2,7 +2,9 @@
 
 namespace SimulationStorm.Utilities.Benchmarking;
 
-public class BenchmarkResult<T>(TimeSpan elapsedTime, T functionResult) : BenchmarkResult(elapsedTime)
+public readonly struct BenchmarkResult<T>(TimeSpan elapsedTime, T functionResult)
 {
+    public TimeSpan ElapsedTime { get; } = elapsedTime;
+    
     public T FunctionResult { get; } = functionResult;
 }
